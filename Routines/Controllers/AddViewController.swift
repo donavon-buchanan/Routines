f30e63f2b44525517616ef4cb208c730ed071aa0
Donavon Buchanan <donavoncade@users.noreply.github.com>
2018-09-27 21:33:37 -0400

    New Icon

---
+++ Routines/Controllers/AddViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddViewController.swift
+//  Routines
+//
+//  Created by Donavon Buchanan on 9/27/18.
+//  Copyright © 2018 Donavon Buchanan. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        let destination = segue.destination as! TableViewController
+        destination.tableView.reloadData()
+    }
+    
+
+}
