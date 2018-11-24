f75b51cd5f19a834d957217d2c0f06fdc149787a
Donavon Buchanan <donavoncade@users.noreply.github.com>
2018-11-23 20:54:37 -0500

    Fixed colors

--- Routines/GlobalPicker.swift
+++ Routines/GlobalPicker.swift
@@ -28,10 +28,10 @@ enum GlobalPicker {
     
     static let textInputBackground: ThemeColorPicker = ["#fff", "#fff", "#fff", "#393e46", "#393e46", "#393e46", "#393e46", "#393e46", "#393e46"]
     
-    static let gear = ThemeImagePicker(arrayLiteral: "gear", "gear", "gear", "gear", "gear-white", "gear-white", "gear-white", "gear-white", "gear-white")
-    static let morning = ThemeImagePicker(arrayLiteral: "morning", "morning", "morning", "morning", "morning-white", "morning-white", "morning-white", "morning-white", "morning-white")
-    static let afternoon = ThemeImagePicker(arrayLiteral: "afternoon", "afternoon", "afternoon", "afternoon", "afternoon-white", "afternoon-white", "afternoon-white", "afternoon-white", "afternoon-white")
-    static let evening = ThemeImagePicker(arrayLiteral: "evening", "evening", "evening", "evening", "evening-white", "evening-white", "evening-white", "evening-white", "evening-white")
-    static let night = ThemeImagePicker(arrayLiteral: "night", "night", "night", "night", "night-white", "night-white", "night-white", "night-white", "night-white")
+    static let gear = ThemeImagePicker(arrayLiteral: "gear", "gear", "gear", "gear-white", "gear-white", "gear-white", "gear-white", "gear-white", "gear-white")
+    static let morning = ThemeImagePicker(arrayLiteral: "morning", "morning", "morning", "morning-white", "morning-white", "morning-white", "morning-white", "morning-white", "morning-white")
+    static let afternoon = ThemeImagePicker(arrayLiteral: "afternoon", "afternoon", "afternoon", "afternoon-white", "afternoon-white", "afternoon-white", "afternoon-white", "afternoon-white", "afternoon-white")
+    static let evening = ThemeImagePicker(arrayLiteral: "evening", "evening", "evening", "evening-white", "evening-white", "evening-white", "evening-white", "evening-white", "evening-white")
+    static let night = ThemeImagePicker(arrayLiteral: "night", "night", "night", "night-white", "night-white", "night-white", "night-white", "night-white", "night-white")
     //static let shadowImages = ThemeImagePicker(images: UIImage(), UIImage(), UIImage(), UIImage())
 }
