adb793cb1600c3b36e7ae1713d8c34457e3c5918
Donavon Buchanan <donavoncade@users.noreply.github.com>
2019-04-27 05:44:46 -0400

    Hopefully this meets app store requirements now

--- Routines/Controllers/RoutinesPlusViewController.swift
+++ Routines/Controllers/RoutinesPlusViewController.swift
@@ -14,7 +14,8 @@ class RoutinesPlusViewController: UIViewController {
     @IBOutlet var routinesPlusLabel: UILabel!
     @IBOutlet var routinesLabelPlusSymbol: UILabel!
 
-    @IBOutlet var yearlyBreakdownLabel: UILabel!
+    @IBOutlet var yearlyDescriptionLabel: UILabel!
+    @IBOutlet var monthlyDescriptionLabel: UILabel!
 
     @IBOutlet var paymentButtons: [UIButton]!
 
@@ -23,13 +24,13 @@ class RoutinesPlusViewController: UIViewController {
     @IBOutlet var lifetimeButton: UIButton!
 
     @IBOutlet var restoreButton: UIButton!
-    
-    @IBOutlet weak var lifetimeTermsLabel: UILabel!
-    @IBOutlet weak var subscriptionTermsLabel: UILabel!
-    
-    @IBOutlet weak var privacyPolicyButton: UIButton!
-    @IBOutlet weak var termsOfServiceButton: UIButton!
-    
+
+    @IBOutlet var lifetimeTermsLabel: UILabel!
+    @IBOutlet var subscriptionTermsLabel: UILabel!
+
+    @IBOutlet var privacyPolicyButton: UIButton!
+    @IBOutlet var termsOfServiceButton: UIButton!
+
     @IBAction func restoreButtonTapped(_: UIButton) {
         restorePurchase()
     }
@@ -45,12 +46,17 @@ class RoutinesPlusViewController: UIViewController {
     @IBAction func lifetimeButtonTapped(_: UIButton) {
         purchase(purchase: .lifetime)
     }
-    
+
     @IBOutlet var policyButtons: [UIButton]!
-    
-    @IBAction func privacyPolicyButtonTapped(_ sender: UIButton) {
+
+    @IBAction func privacyPolicyButtonTapped(_: UIButton) {
+        guard let url = URL(string: "https://donavon.app/privacy-policy") else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
-    @IBAction func termsOfServiceButtonTapped(_ sender: UIButton) {
+
+    @IBAction func termsOfServiceButtonTapped(_: UIButton) {
+        guard let url = URL(string: "https://donavon.app/tos") else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
 
     override func viewDidLoad() {
@@ -64,8 +70,19 @@ class RoutinesPlusViewController: UIViewController {
     }
 
     var monthlyPrice = ""
+    var monthlySubtext: String {
+        return "1 week free trial, then \(monthlyPrice) / mo. after the trial has ended"
+    }
+
     var yearlyPrice = ""
+    var yearlySubtext: String {
+        return "2 week free trial, then \(yearlyPrice) / yr. after the trial has ended \n(12 months at \(twelveMonthPrice) / mo. Save 50%)"
+    }
+
     var lifetimePrice = ""
+//    var lifetimeSubtext: String {
+//        return ""
+//    }
     var twelveMonthPrice: String {
         let priceString = yearlyPrice.dropFirst()
         let priceDouble = Double(priceString)
@@ -79,6 +96,10 @@ class RoutinesPlusViewController: UIViewController {
         } else {
             return "$0.49"
         }
+    }
+
+    var subscriptionTermsString: String {
+        return "For new subscribers, monthly subscription includes a 1 week free trial, or 2 weeks for the yearly subscription. At the end of your free trial, your Apple ID account will be billed \(monthlyPrice) for monthly subscription, or \(yearlyPrice) for yearly subscription unless your subscription is canceled at least 24 hours before the end of the free trial period. If you continue your subscription, your account will automatically be charged for renewal unless canceled at least 24 hours prior to the end of the current subscription period. You can manage and cancel your subscriptions by going to your App Store account settings after purchase."
     }
 
     func getPrices() {
@@ -100,7 +121,9 @@ class RoutinesPlusViewController: UIViewController {
         monthlyButton.setTitle("\(monthlyPrice) / Month", for: .normal)
         yearlyButton.setTitle("\(yearlyPrice) / Year", for: .normal)
         lifetimeButton.setTitle("\(lifetimePrice) / Lifetime", for: .normal)
-        yearlyBreakdownLabel.text = "(12 months at \(twelveMonthPrice) / mo. Save 50%)"
+
+        monthlyDescriptionLabel.text = monthlySubtext
+        yearlyDescriptionLabel.text = yearlySubtext
     }
 
     func setUpUI() {
@@ -119,14 +142,14 @@ class RoutinesPlusViewController: UIViewController {
             button.layer.cornerRadius = 12
             button.backgroundColor = titleColor
         }
-        
+
         let policyColor = UIColor(red: 0.05, green: 0.30, blue: 0.57, alpha: 1.00)
         policyButtons.forEach { button in
             button.layer.masksToBounds = true
             button.layer.cornerRadius = 20
             button.backgroundColor = policyColor
         }
-        
+
         yearlyButton.layer.shadowColor = shadowColor.cgColor
         yearlyButton.layer.shadowRadius = 16
         yearlyButton.layer.shadowOpacity = 1
@@ -136,5 +159,7 @@ class RoutinesPlusViewController: UIViewController {
         restoreButton.setTitleColor(titleColor, for: .normal)
 
         setButtonText()
+
+        subscriptionTermsLabel.text = subscriptionTermsString
     }
 }
