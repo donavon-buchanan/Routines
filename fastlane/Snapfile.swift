3b20986a5b8b55510bd6fd275cd67579b024e17e
Donavon Buchanan <donavoncade@users.noreply.github.com>
2019-05-09 05:42:39 -0400

    fastlane snapshot setup

---
+++ fastlane/Snapfile.swift
@@ -0,0 +1,31 @@
+//// Uncomment the lines below you want to change by removing the // in the beginning
+//
+// class Snapshotfile: SnapshotfileProtocol {
+//    // A list of devices you want to take the screenshots from
+//    var devices: [String] = [“iPhone 6”, “iPhone 6 Plus”, “iPhone X”, “iPhone Xs Max”, “iPad Pro (12.9-inch) (2nd generation)”, “iPad Pro (12.9-inch) (3rd Generation)”, “iPad Pro (9.7-inch)”]
+//
+//    // locales not supported in Swift yet
+//    var languages: [String] { return [
+//        "en-US"
+//    ]
+//    }
+//
+//    // The name of the scheme which contains the UI Tests
+//    var scheme: String? { return "RoutinesUITests" }
+//
+//    // Where should the resulting screenshots be stored?
+//    var outputDirectory: String { return "./screenshots" }
+//
+//    // Clear all previously generated screenshots before creating new ones
+//    var clearPreviousScreenshots: Bool { return true }
+//
+//    // Choose which project/workspace to use
+//    // var project: String? { return "./Project.xcodeproj" }
+//    var workspace: String? { return ./Routines.xcworkspace }
+//
+//    // Arguments to pass to the app on launch. See https://docs.fastlane.tools/actions/snapshot/#launch-arguments
+//    // var launchArguments: [String] { return ["-favColor red"] }
+//
+//    // For more information about all available options run
+//    // fastlane snapshot --help
+// }
