94de7b4babbd8898a41b50cd3c49aca770e25247
Donavon Buchanan <donavoncade@users.noreply.github.com>
2019-05-09 03:26:21 -0400

    Setting up snapshots

--- RoutinesUITests/RoutinesUITests.swift
+++ RoutinesUITests/RoutinesUITests.swift
@@ -16,7 +16,9 @@ class RoutinesUITests: XCTestCase {
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         
@@ -31,33 +33,46 @@ class RoutinesUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
         let app = XCUIApplication()
+        snapshot("0Launch")
         let tabBarsQuery = app.tabBars
         let afternoonButton = tabBarsQuery.buttons["Afternoon"]
         afternoonButton.tap()
-        
+        snapshot("0Launch")
         let button = app.navigationBars["Afternoon"].children(matching: .button).element(boundBy: 2)
         button.tap()
-        
+        snapshot("0Launch")
         let tablesQuery = app.tables
         let askJenniferWhereSheDLikeToHaveDinnerStaticText = tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Ask Jennifer where she’d like to have dinner"]/*[[".cells.staticTexts[\"Ask Jennifer where she’d like to have dinner\"]",".staticTexts[\"Ask Jennifer where she’d like to have dinner\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         askJenniferWhereSheDLikeToHaveDinnerStaticText.swipeLeft()
-        
+        snapshot("0Launch")
         let button2 = app.navigationBars["All Day"].children(matching: .button).element(boundBy: 2)
         button2.tap()
-        
+        snapshot("0Launch")
         let eveningButton = tabBarsQuery.buttons["Evening"]
         eveningButton.tap()
+        snapshot("0Launch")
         tabBarsQuery.buttons["Night"].tap()
+        snapshot("0Launch")
         app.navigationBars["Night"].buttons["Options"].tap()
+        snapshot("0Launch")
         app/*@START_MENU_TOKEN@*/.tables.containing(.other, identifier:"START TIMES").element/*[[".tables.containing(.other, identifier:\"ROUTINES+\").element",".tables.containing(.other, identifier:\"STYLE\").element",".tables.containing(.other, identifier:\"Enable to receive notifications at the start of each period\").element",".tables.containing(.other, identifier:\"NOTIFICATIONS\").element",".tables.containing(.other, identifier:\"Set when each time period should begin\").element",".tables.containing(.other, identifier:\"START TIMES\").element"],[[[-1,5],[-1,4],[-1,3],[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeUp()
+        snapshot("0Launch")
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Dark Mode"]/*[[".cells.staticTexts[\"Dark Mode\"]",".staticTexts[\"Dark Mode\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        snapshot("0Launch")
         app.navigationBars["Settings"].buttons["Done"].tap()
+        snapshot("0Launch")
         eveningButton.tap()
+        snapshot("0Launch")
         afternoonButton.tap()
+        snapshot("0Launch")
         button.tap()
+        snapshot("0Launch")
         askJenniferWhereSheDLikeToHaveDinnerStaticText.swipeLeft()
+        snapshot("0Launch")
         button2.tap()
+        snapshot("0Launch")
         tabBarsQuery.buttons["Morning"].tap()
+        snapshot("0Launch")
         
     }
 }
